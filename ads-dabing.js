2a73d1b4873d2690f62c46f505e4746cc0101f75
valikden <57502334+valikden@users.noreply.github.com>
2026-08-13 17:26:47 +0200

    Update ads-dabing.js

--- ads-dabing.js
+++ ads-dabing.js
@@ -1,38 +1,39 @@
 (function () {
     'use strict';
 
-    function initPlugin() {
-        // 1. Додаємо новий розділ меню в Налаштування
-        Lampa.SettingsApi.addComponent({
-            component: 'ads_dabing',
-            name: 'ADS-Dabing',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="currentColor"/></svg>'
-        });
+    function addSettingsRow() {
+        // Чекаємо відкриття налаштувань
+        Lampa.Settings.listener.follow('open', function (e) {
+            if (e.name === 'main') {
+                // Перевіряємо чи вже додано
+                if (!e.body.find('.ads-dabing-row').length) {
+                    
+                    var html = `
+                        <div class="settings-param selector ads-dabing-row" data-type="button">
+                            <div class="settings-param__name">ADS-Dabing</div>
+                            <div class="settings-param__value">Працює</div>
+                        </div>
+                    `;
 
-        // 2. Додаємо перемикач у цей розділ
-        Lampa.SettingsApi.addParam({
-            component: 'ads_dabing',
-            param: {
-                name: 'ads_dabing_enabled',
-                type: 'trigger',
-                default: true
-            },
-            field: {
-                name: 'Статус плагіна',
-                description: 'Увімкнути або вимкнути озвучку ADS-Dabing'
-            },
-            onChange: function (value) {
-                Lampa.Noty.show('ADS-Dabing: ' + (value ? 'Увімкнено' : 'Вимкнено'));
+                    var $item = $(html);
+
+                    $item.on('hover:enter', function () {
+                        Lampa.Noty.show('Плагін ADS-Dabing активний!');
+                    });
+
+                    // Вставляємо в кінець списку налаштувань
+                    e.body.find('.settings-param').last().after($item);
+
+                    // Оновлюємо навіграцію пульта/клавіатури Lampa
+                    if (window.Lampa && Lampa.Controller) {
+                        Lampa.Controller.enable('settings_main');
+                    }
+                }
             }
         });
     }
 
-    // Запуск плагіна
-    if (window.Lampa && Lampa.SettingsApi) {
-        initPlugin();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') initPlugin();
-        });
+    if (window.Lampa) {
+        addSettingsRow();
     }
 })();
